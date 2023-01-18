f653367474f3ca47f761468b9b7239066509141e
Habiba Mahmoud <habibaabouelyazeed@gmail.com>
2023-01-18 18:26:10 +0200

    boarders action glow

--- script.js
+++ script.js
@@ -29,25 +29,48 @@ function computerChoice(){
     }
 }
 
+function choiceToID(userChoice){
+    switch(userChoice){
+        case 'Rock': return rockChoice;
+        case 'Paper': return paperChoice;
+        case 'Scissors': return scissorsChoice;
+    }
+}
+
 //evaluate game scenarios
 
 function win(userChoice, compChoice){
+    let choice = choiceToID(userChoice)
     initUserScore++;
     userScore.innerHTML = initUserScore;
     let msg = `${userChoice} beats ${compChoice}. You win! &#128165`
     resultMsg.innerHTML = msg;
+    choice.classList.add('greenGlow')
+    setTimeout(() => {
+        choice.classList.remove('greenGlow')
+    }, 400);
 }
 
 function lose(userChoice, compChoice){
+    let choice = choiceToID(userChoice)
     initCompScore++;
     compScore.innerHTML = initCompScore;
     let msg = `${userChoice} loses to ${compChoice}. You lose! &#10060`
     resultMsg.innerHTML = msg;
+    choice.classList.add('redGlow')
+    setTimeout(() => {
+        choice.classList.remove('redGlow')
+    }, 400);
 }
 
 function draw(userChoice, compChoice){
+    let choice = choiceToID(userChoice)
     let msg = `${compChoice} equals ${userChoice}. It's a draw! &#127775`
     resultMsg.innerHTML = msg;
+    choice.classList.add('greyGlow')
+    setTimeout(() => {
+        choice.classList.remove('greyGlow')
+    }, 400);
 }
 
 function game(userChoice){
